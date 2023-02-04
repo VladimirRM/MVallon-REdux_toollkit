71477a4dbc956b7183442f3ec5cf3dd805e3f685
Vladimir RM <shvdrummer@gmail.com>
2023-02-04 15:15:19 +0200

    create reducers

--- src/store/slices/counterSkices.js
+++ src/store/slices/counterSkices.js
@@ -3,3 +3,16 @@ import { createSlice  } from "@reduxjs/toolkit";
 const initialState = {
     counter: 0
 }
+
+const counterSlice = createSlice({
+     name:'counter' ,
+     initialState:initialState,
+     reducers:{
+        incremented:state =>{
+            state.counter += 1
+        },
+        decremented:state =>{
+            state.counter -= 1
+        },
+     }
+})
