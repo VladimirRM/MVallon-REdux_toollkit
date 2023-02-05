353c6370fbe91387f2ef70906e0bb4e5f06225bc
Vladimir RM <shvdrummer@gmail.com>
2023-02-05 13:37:12 +0200

    Create const dispatch & count

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import { useSelector,useDispatch } from 'react-redux';
 import './App.css';
 
 function App() {
+
+  const count = useSelector()
+  const dispatch = useDispatch()
   return (
     <div className="App">
-   
+   <h1></h1>
+   <button onClick={()=>{}}>Increment</button>
+   <button onClick={()=>{}}>Decrement</button>
     </div>
   );
 }
