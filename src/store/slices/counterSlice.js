6a0531a977f7d5204796db813009dce9e360528e
Vladimir RM <shvdrummer@gmail.com>
2023-02-05 20:48:20 +0200

    add export default counterSlice

--- src/store/slices/counterSlice.js
+++ src/store/slices/counterSlice.js
@@ -1,24 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const initialState ={
-    counter:0
+    counter: 0
 }
 
+
 const counterSlice = createSlice({
-    name: counter,
-    initialState: initialState,
+    name:counter,
+    initialState:initialState,
     reducers:{
-        incremented:(state)=>{
-            state.counter +=1
-        },
-        decremented:(state)=>{
-            state.counter -+1
-        }
-    }
+        incremented:(state)=>
+        state.counter +=1
+    },
+    decremented:(state)=>
+    state.counter -=1
 })
-export const counterSelector ={
-    getCounter:(state)=state.counter
+export const counterSelector= {
+    getCounter:(state)=>state.counter
 }
 export const {incremented,decremented}= counterSlice.actions
 export default counterSlice
--- src/store/slices/counterSlices1.js
+++ src/store/slices/counterSlices1.js
@@ -8,16 +8,14 @@ const counterSlice = createSlice({
   name: counter,
   initialState: initialState,
   reducers: {
-    incremented: (state) => {
-      state.counter += 1;
-    },
-    decremented: (state) => {
-      state.counter -= 1;
-    },
+    incremented: (state) => (state.counter += 1),
   },
+  decremented: (state) => (state.counter -= 1),
 });
+
 export const counterSelector = {
   getCounter: (state) => state.counter,
 };
-export  const { incremented, decremented } = counterSlice.actions;
-export default counterSlice
+
+export const { incremented, decremented } = counterSlice.actions;
+export default counterSlice;
